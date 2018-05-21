dad570f30206f8e1b0f7df11375fcbb828e4b45c
Abepena <pena.abe@gmail.com>
2018-05-21 19:20:48 -0400

    working DRF generic view for Votes

--- votes/views.py
+++ votes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.views import generics
+from rest_framework import generics
 from .models import Vote
 from .serializers import VoteSerializer
 # Create your views here.
